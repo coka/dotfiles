7c9fe37a46894146172e988b246a6ed862ad4ecf
Bojan Čoka <bojan.coka@gmail.com>
2026-01-17 16:30:38 +0100

    Format with Alejandra

--- home.nix
+++ home.nix
@@ -1,6 +1,8 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "coka";
@@ -18,9 +20,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs.alejandra
+    pkgs.gnumake
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
